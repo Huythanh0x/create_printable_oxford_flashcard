47fd128f802100b169586cd24649da5acb3612fd
huythanh0x <huythanh0x@gmail.com>
2022-01-28 01:08:18 +0700

    get font size to avoid hole in flashcard

--- font/get_width_sentence.py
+++ font/get_width_sentence.py
@@ -1,10 +1,11 @@
 from fontTools.ttLib import TTFont
 from fontTools.ttLib.tables._c_m_a_p import CmapSubtable
+from pandas import describe_option
 
-MAX_WIDTH = 265
+MAX_WIDTH = 310
 TEXT_SIZE = 14
-
-font = TTFont('font/Roboto-400.ttf')
+##init font
+font = TTFont('font/Voces-Regular.ttf')
 cmap = font['cmap']
 t = cmap.getcmap(3,1).cmap
 s = font.getGlyphSet()
@@ -20,53 +21,19 @@ def getTextWidth(text,pointSize):
     total = total*float(pointSize)/units_per_em;
     return total
 
-text = 'a person who works to achieve political or social change, especially as a member of an organization with particular aims'
-list_splitted_text = text.split(' ')
-start_index_line = 0
-for i,word in enumerate(list_splitted_text):
-    if getTextWidth(" ".join(list_splitted_text[start_index_line:i]),TEXT_SIZE) < MAX_WIDTH:
-        pass
-    else:
-        print(" ".join(list_splitted_text[start_index_line:i-1]))
-        print(getTextWidth(" ".join(list_splitted_text[start_index_line:i-1]),TEXT_SIZE))
-        start_index_line = i-1
+def get_split_description(text):
+    list_splitted_text = text.split(' ')
+    start_index_line = 0
+    description = []
+    for i,word in enumerate(list_splitted_text):
+        if getTextWidth(" ".join(list_splitted_text[start_index_line:i]),TEXT_SIZE) <= MAX_WIDTH:
+            pass
+        else:
+            description +=  [" ".join(list_splitted_text[start_index_line:i-1])]
+            start_index_line = i-1
+    description += [" ".join(list_splitted_text[start_index_line:i+1])]
 
-# text = "language that shows which country, area"
-# width = getTextWidth(text,14)
-# print ('Text: "%s"' % text)
-# print ('Width in points: %f' % width)
-# print(len(text))
-print("-------------------------------------------------------")
-print(getTextWidth("a way of pronouncing the words of a",14))
-print(getTextWidth(" language that shows which country, area",14))
-print(getTextWidth("or social class a person comes from; how",14))
-print(getTextWidth("welll somebody pronounces a particular",14))
-print(getTextWidth("the fact of somebody being away from a",14))
-print(getTextWidth("place where they are usually expected to",14))
-print(getTextWidth("the fact of being responsible for your",14))
-print(getTextWidth("decisions or actions and expected to",14))
-print(getTextWidth("responsible for your decisions or actions",14))
-print(getTextWidth("the condition of being unable to stop using",14))
-print(getTextWidth("or doing something as a habit, especially",14))
-print(getTextWidth("something, or of being changed, to suit a",14))
-print(getTextWidth("and expected to explain them when you",14))
-print("-------------------------------------------")
-print(getTextWidth("a way of pronouncing the words of a language",14))
-print(getTextWidth(" language that shows which country, area or",14))
-print(getTextWidth("or social class a person comes from; how well",14))
-print(getTextWidth("welll somebody pronounces a particular language",14))
-print(getTextWidth("the fact of somebody being away from a place",14))
-print(getTextWidth("place where they are usually expected to be;",14))
-print(getTextWidth("the fact of being responsible for your decisions",14))
-print(getTextWidth("decisions or actions and expected to explain",14))
-print(getTextWidth("responsible for your decisions or actions and",14))
-print(getTextWidth("the condition of being unable to stop using or",14))
-print(getTextWidth("or doing something as a habit, especially something",14))
-print(getTextWidth("something, or of being changed, to suit a new",14))
-print(getTextWidth("and expected to explain them when you are",14))
+    return description
 
-
-
-# print ('Width in inches: %f' % (width/72))
-# print ('Width in cm: %f' % (width*2.54/72))
-# print ('Width in WP Units: %f' % (width*1200/72))
+split_description = get_split_description('a long piece of cloth with a message on it that is carried between two poles or hung in a public place to show support foor something')
+print(split_description)
--- html_flashcard_generator.py
+++ html_flashcard_generator.py
@@ -1,4 +1,9 @@
 import math
+from fontTools.ttLib import TTFont
+
+
+MAX_WIDTH = 280 
+TEXT_SIZE = 14
 NUMBER_CARD_PER_PAGE = 20
 NUMBER_CARD_PER_ROW = 4
 MARGIN_RIGHT = 20
@@ -8,6 +13,42 @@ class HTMLFlashCardGenerator():
         self.file_name = file_name
         self.NUMBER_CARD_PER_PAGE = NUMBER_CARD_PER_PAGE
         self.NUMBER_CARD_PER_ROW = NUMBER_CARD_PER_ROW
+        self.MAX_WIDTH = MAX_WIDTH
+        self.TEXT_SIZE = TEXT_SIZE
+        ##init font
+        font = TTFont('font/Voces-Regular.ttf')
+        cmap = font['cmap']
+        self.t = cmap.getcmap(3,1).cmap
+        self.s = font.getGlyphSet()
+        self.units_per_em = font['head'].unitsPerEm
+
+    def getTextWidth(self,text,pointSize):
+        total = 0
+        for c in text:
+            if ord(c) in self.t and self.t[ord(c)] in self.s:
+                total += self.s[self.t[ord(c)]].width
+            else:
+                total += self.s['.notdef'].width
+        total = total*float(pointSize)/self.units_per_em;
+        return total
+    
+    def get_split_description(self,text):
+        list_splitted_text = text.split(' ')
+        start_index_line = 0
+        description = []
+        for i,_ in enumerate(list_splitted_text):
+            if self.getTextWidth(" ".join(list_splitted_text[start_index_line:i]),TEXT_SIZE) <= MAX_WIDTH:
+                pass
+            else:
+                description +=  [" ".join(list_splitted_text[start_index_line:i-1])]
+                start_index_line = i-1
+        description += [" ".join(list_splitted_text[start_index_line:i+1])]
+        ###test
+        if len(description)>3:
+            description += ["ffff"]
+
+        return description
+
 
     def create_html_flash_card(self):
         with open(f'data_csv/{self.file_name}.csv','r') as f:
@@ -15,34 +56,45 @@ class HTMLFlashCardGenerator():
 
         number_of_word = len(word_list)
         number_of_page = math.ceil(len(word_list)/NUMBER_CARD_PER_PAGE)
-        number_of_card = number_of_page * NUMBER_CARD_PER_PAGE
         with open(f'output_html/{self.file_name}.html','w') as f:
-            style_css = "@media print {   .pagebreak { page-break-before: always; } /* page-break-after works, as well */}*{    margin: 0;}.container{    position: relative;    margin: 0px "+ str(MARGIN_RIGHT) +"px 0px 0px;    display: flex;}.front_card{    margin-left:0px;    width: 283.4px;    height: 154.4px;    background: #FFFFFF;    border-radius: 0px;    border: black 2px solid;    position: relative;}.front_card .type_of_word{position: absolute;width: auto;padding:3px 5px;height: 12px;font-family: sans-serif;left: 0px;top: 0px;background: #FFFFFF;border-radius: 15px;border: black 2px solid;font-style: normal;font-weight: normal;font-size: 12px;line-height: 14px;color: #000000;text-align: center;}.front_card .word{position: absolute;width: auto;height: 19px;top: 28px;margin: 0px 0px 0px;left:50%;transform: translateX(-50%);font-family: 'Montserrat', sans-serif;font-style: normal;font-weight: 700;font-size: 26px;line-height: 30px;color: #0798D6;}.front_card .describe{    position: absolute;width: 94%;height: auto;left: 50%;transform: translateX(-50%);top: 75px;font-family: sans-serif;font-style: normal;font-weight: normal;font-size: 14px;line-height: 110%;color: #000000;}.back_card{margin-left:0px;width: 283.4px;height: 154.4px;background: #FFFFFF;border-radius: 0px;border: black 2px solid;position: relative;transform: rotate(-180deg);}.back_card .ipa{    position: absolute;    width: max-content;    height: 19px;    top: 15px;    left: 50%;    font-family: 'Montserrat', sans-serif;    font-style: normal;    font-weight: 700;    font-size: 26px;    line-height: 30px;    transform: translateX(-50%);    color: #0798D6;}.back_card .example{    font-family: sans-serif;    margin-bottom: 8px;    font-style: normal;    font-weight: normal;    font-size: 14px;    line-height: 110%;    color: #000000;}.container_example{    position: absolute;    width: 94%;    height: auto;    left: 50%;    transform: translateX(-50%);    top: 60px;}.footer{    font-family: sans-serif;    margin-bottom: 8px;    margin-right: 5px;    position: absolute;    font-style: normal;    font-weight: normal;    font-size: 10px;    line-height: 0;    color: #000000;    bottom: 0;    right: 0;}"
+            # style_css = "@media print {   .pagebreak { page-break-before: always; } /* page-break-after works, as well */}*{    margin: 0;}.container{    position: relative;    margin: 0px "+ str(MARGIN_RIGHT) +"px 0px 0px;    display: flex;}.front_card{    margin-left:0px;    width: 283.4px;    height: 154.4px;    background: #FFFFFF;    border-radius: 0px;    border: black 2px solid;    position: relative;}.front_card .type_of_word{position: absolute;width: auto;padding:3px 5px;height: 12px;font-family: sans-serif;left: 0px;top: 0px;background: #FFFFFF;border-radius: 15px;border: black 2px solid;font-style: normal;font-weight: normal;font-size: 12px;line-height: 14px;color: #000000;text-align: center;}.front_card .word{position: absolute;width: auto;height: 19px;top: 28px;margin: 0px 0px 0px;left:50%;transform: translateX(-50%);font-family: 'Montserrat', sans-serif;font-style: normal;font-weight: 700;font-size: 26px;line-height: 30px;color: #0798D6;}.front_card .describe{    position: absolute;width: 94%;height: auto;left: 50%;transform: translateX(-50%);top: 75px;font-family: sans-serif;font-style: normal;font-weight: normal;font-size: 14px;line-height: 110%;color: #000000;}.back_card{margin-left:0px;width: 283.4px;height: 154.4px;background: #FFFFFF;border-radius: 0px;border: black 2px solid;position: relative;transform: rotate(-180deg);}.back_card .ipa{    position: absolute;    width: max-content;    height: 19px;    top: 15px;    left: 50%;    font-family: 'Montserrat', sans-serif;    font-style: normal;    font-weight: 700;    font-size: 26px;    line-height: 30px;    transform: translateX(-50%);    color: #0798D6;}.back_card .example{    font-family: sans-serif;    margin-bottom: 8px;    font-style: normal;    font-weight: normal;    font-size: 14px;    line-height: 110%;    color: #000000;}.container_example{    position: absolute;    width: 94%;    height: auto;    left: 50%;    transform: translateX(-50%);    top: 60px;}.footer{    font-family: sans-serif;    margin-bottom: 8px;    margin-right: 5px;    position: absolute;    font-style: normal;    font-weight: normal;    font-size: 10px;    line-height: 0;    color: #000000;    bottom: 0;    right: 0;}.additional_describe{position: absolute;width: 80%;top:120px;height: auto;left:40px;font-family: sans-serif;font-style: normal;font-weight: normal;font-size: 14px;line-height: 110%;color: #000000;}.back_card{margin-left:0px;width: 283.4px;height: 154.4px;background: #FFFFFF;border-radius: 0px;border: black 2px solid;position: relative;transform: rotate(-180deg);}"
             
-            f.writelines(f'<!DOCTYPE html><html lang="en"><head><style>{style_css}</style><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>{self.file_name}</title><link rel="stylesheet" href="./index.css"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500;700&display=swap" rel="stylesheet"></head><body>')
+            f.writelines(f'<!DOCTYPE html><html lang="en"><head>    <link rel="stylesheet" href="./index.css"><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>{self.file_name}</title><link rel="stylesheet" href="./index.css"><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500;700&display=swap" rel="stylesheet"></head><body>')
 
         with open(f'output_html/{self.file_name}.html','a') as f:
             for page_index in range(number_of_page):
-                for row in range(NUMBER_CARD_PER_PAGE//NUMBER_CARD_PER_ROW):
-                    f.writelines('<div class="container">')
-                    base_index = page_index*NUMBER_CARD_PER_PAGE+row*NUMBER_CARD_PER_ROW
-                    for card in range(NUMBER_CARD_PER_ROW):
-                        card_index = base_index + card    
-                        if card_index < number_of_word:
-                            type_of_speech = word_list[card_index].split('|')[2]
-                            en_word = word_list[card_index].split('|')[1]
-                            description = word_list[card_index].split('|')[4]
-                            if len(description) > 100:
-                                description = description.split('.')[0]
-                            if len(description) > 120:
-                                description = description[:121]  + description[120:]
-                                print(description)
-                        else:
-                            type_of_speech = "noun"
-                            en_word = "BatDauLapTrinh"
-                            description = "A blog where I share my career journey and some learnings I picked up along the way"
-                        f.write(f'<div class="front_card"><span class="type_of_word">{type_of_speech}</span><h2 class="word">{en_word}</h2><p class="describe">{description}</p></div>')
-                    f.writelines('</div>')
+                # for row in range(NUMBER_CARD_PER_PAGE//NUMBER_CARD_PER_ROW):
+                #     f.writelines('<div class="container">')
+                #     base_index = page_index*NUMBER_CARD_PER_PAGE+row*NUMBER_CARD_PER_ROW
+                #     for card in range(NUMBER_CARD_PER_ROW):
+                #         card_index = base_index + card    
+                #         if card_index < number_of_word:
+                #             type_of_speech = word_list[card_index].split('|')[2]
+                #             en_word = word_list[card_index].split('|')[1]
+                #             description = word_list[card_index].split('|')[4]
+                #             if len(description) > 170:
+                #                 description = description.split('.')[0]
+                #             if len(description) > 170:
+                #                 description = description.split(';')[0]
+                #             split_description = self.get_split_description(description)
+                #             main_description = ""
+                #             additional_description = ""
+                #             for i,_ in enumerate(split_description):
+                #                 if i < 3:
+                #                     main_description += " " + split_description[i]
+                #                 else:
+                #                     additional_description += " " + split_description[i]
+
+                #         else:
+                #             type_of_speech = "noun"
+                #             en_word = "BatDauLapTrinh"
+                #             description = "A blog where I share my career journey and some learnings I picked up along the way"
+                #         if len(description) > 170:
+                #             front_card_html = f'<div class="front_card"><span class="type_of_word">{type_of_speech}</span><h2 class="word">{en_word}</h2><p class="describe" style="top:55px;">{main_description}</p><p class="additional_describe" style="top:100px;">{additional_description}</p></div>'
+                #         else:
+                #             front_card_html = f'<div class="front_card"><span class="type_of_word">{type_of_speech}</span><h2 class="word">{en_word}</h2><p class="describe">{main_description}</p><p class="additional_describe">{additional_description}</p></div>'
+                #         f.write(front_card_html)
+                #     f.writelines('</div>')
                 
                 for row in range(NUMBER_CARD_PER_PAGE//NUMBER_CARD_PER_ROW):
                     f.writelines('<div class="container">')
